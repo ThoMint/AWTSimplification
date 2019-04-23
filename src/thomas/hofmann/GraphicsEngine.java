e1b39fe3e2bdd7796b03e7af58358a68d6bf6b4f
Thomas <thomas.hofmann2003@gmail.com>
2019-04-23 10:25:04 +0200

    Initialize Repo

---
+++ src/thomas/hofmann/GraphicsEngine.java
@@ -0,0 +1,107 @@
+package thomas.hofmann;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+
+import javax.swing.JPanel;
+
+
+
+@SuppressWarnings("serial")
+//Handles drawing and user input
+public class GraphicsEngine extends Thread
+{
+	public int width,height;
+	private double wait,diff,start;
+	//Interface
+	private Engine e;
+	private Window w;
+	private JPanel p = new JPanel()
+	{
+		@Override
+		public void paint(Graphics g)
+		{
+			super.paint(g);
+			super.paintComponent(g);
+			//Draw the background
+			g.setColor(Color.LIGHT_GRAY);
+			g.fillRect(0, 0,this.getWidth(), this.getHeight());
+			g.setColor(Color.BLACK);
+			//Create another graphics object and flip the coordinate System
+			Graphics2D g2 = (Graphics2D) g.create();
+			AffineTransform tform = AffineTransform.getTranslateInstance(0,this.getHeight());
+			tform.scale(1, -1);
+			g2.setTransform(tform);
+			//Call the draw function from the interface determined by the user
+			e.draw(g2);
+		}
+	};
+	
+	//Constructor
+	public GraphicsEngine(Window w,Engine e)
+	{
+		super();
+		//Add the JPanel to the JFrame (Window)
+		w.add(p);
+		w.validate();
+		p.validate();
+		w.validate();
+		//Focus is needed for user input
+		p.setFocusable(true);
+		p.requestFocus();
+		p.setIgnoreRepaint(false);
+		//Add listeners for user input
+		p.addKeyListener(e);
+		p.addMouseMotionListener(e);
+		p.addMouseListener(e);
+		this.w=w;
+		this.e=e;
+	}
+	
+	//To get a stable frame rate
+	public void capFrameRate(double fps) {
+		//Calculate standard waiting time in milliseconds
+	    wait = 1000 / fps;
+	    //The time passed since the last 
+	    diff = System.currentTimeMillis() - start;
+	    //If the computer can keep up with the frame rate lock it to the
+	    //desired rate
+	    if (diff < wait)
+	    {
+	        try 
+	        {
+				Thread.sleep((long)(wait - diff));
+			}
+	        catch (InterruptedException e) 
+	        {
+				e.printStackTrace();
+			}
+	    }
+	    //Set new starting time to measure time
+	    start = System.currentTimeMillis();
+	}
+	
+	//Extra Thread for loop
+	@Override
+	public void run()
+	{
+		super.run();
+		while(true)
+		{
+			//Call the loop function through the interface
+			this.e.loop(this);
+			this.width=p.getWidth();
+			this.height=p.getHeight();
+		}
+	}
+	
+	//Calls the repaint function of the JPanel
+	public void redraw()
+	{
+		w.validate();
+		p.validate();
+		p.repaint();
+	}
+}
---
+++ src/thomas/hofmann/Window.java
@@ -0,0 +1,79 @@
+package thomas.hofmann;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Window extends JFrame
+{
+	public int width;
+	public int height;
+	public int x;
+	public int y;
+	public Rectangle bounds;
+	//Resizeable true by default
+	public boolean resizeable=true;
+	
+	//Constructor where you can set resizeable to false
+	public Window(String title, int width, int height, boolean resizeable)
+	{
+		super(title);
+		this.width = width;
+		this.height = height;
+		this.resizeable = resizeable;
+		init();
+	}
+	
+	//Constructor without setting resizeable so it is true
+	public Window(String title, int width, int height)
+	{
+		super(title);
+		this.width = width;
+		this.height = height;
+		init();
+	}
+	
+	//To center the window in the middle of the default screen
+	void getPosition()
+	{
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int width = gd.getDisplayMode().getWidth();
+		int height = gd.getDisplayMode().getHeight();
+		
+		this.x=width/2 - this.width/2;
+		this.y=height/2 - this.height/2;
+	}
+	
+	//The part of the constructor that stays the same for both Variants
+	void init()
+	{
+		//Set the x,y coordinates to center the window
+		getPosition();
+		//Sets the position and size of the frame
+		super.setBounds(x, y, width, height);
+		this.bounds=super.getBounds();
+		super.setVisible(true);
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		super.setResizable(resizeable);
+	}
+	
+	//Update variables when called
+	void update()
+	{
+		this.bounds = super.getBounds();
+		this.width=this.bounds.width;
+		this.height=this.bounds.height;
+		this.x=this.bounds.x;
+		this.y=this.bounds.y;
+	}
+}
+
+
+
+
+
+
+
