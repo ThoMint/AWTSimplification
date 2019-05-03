64ff6352e80738aa2184e117447c272195f16d66
Thomas <thomas.hofmann2003@gmail.com>
2019-05-03 23:10:50 +0200

    Improvements
    Added 'kill' Method for stopping the Thread
    Added 'redrawImmediatly' Method (less performance)

--- src/thomas/hofmann/Engine.java
+++ src/thomas/hofmann/Engine.java
@@ -6,14 +6,14 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 //Interface for User input and draw/loop functions
-public interface Engine extends KeyListener, MouseMotionListener, MouseListener
-{
-	//function in which the user can determine what to draw
-	//by the GraphicsEngine
-	//For details about the capability's of Graphics2D see https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics2D.html
+public interface Engine extends KeyListener, MouseMotionListener, MouseListener {
+	// function in which the user can determine what to draw
+	// by the GraphicsEngine
+	// For details about the capability's of Graphics2D see
+	// https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics2D.html
 	public void draw(Graphics2D g);
-	
-	//function in which the user can lock the frame rate and redraw every frame
-	//Handled by an independent Thread
+
+	// function in which the user can lock the frame rate and redraw every frame
+	// Handled by an independent Thread
 	public void loop(GraphicsEngine ge);
 }
